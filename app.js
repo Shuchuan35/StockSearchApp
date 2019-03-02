abd9d99ff58d4fc04c3d8ede32dadd92a0dcf607
scream <hou.jennifer35@gmail.com>
2019-03-02 14:40:02 -0500

    refactor renderCompanyInfo function

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
   const stocksList = ['FB', 'AAPL', 'TSLA', 'GOOG'];
   const validationList = [];
+
   // create an array for all stock symbols available in iexTrading
   const getAllStockSymbols = function () {
 
@@ -18,55 +19,7 @@ $(document).ready(function () {
   }
   getAllStockSymbols();
 
-  // displaystockInfo function renders the HTML to display the selected company content
-  const displayStockInfo = function () {
-    $('#ceo-view').empty();
-    $('#stocks-view').empty();
-    const stock = $(this).attr('data-name');
-    // Grab the stock symbol from the button clicked and add it to the queryURL
-    const queryURL = `https://api.iextrading.com/1.0/stock/${stock}/batch?types=logo,company,quote,news&range=1m&last=10`;
-
-    $.ajax({
-      url: queryURL,
-      method: 'GET'
-    }).then(function (response) {
-      // Storing the CEO info
-      const ceoDiv = $('<div>');
-      const companyCEO = response.company.CEO;
-      const ceoHolder = $('<small>').text(`CEO: ${companyCEO}`);
-      ceoDiv.append(ceoHolder);
-      $('#ceo-view').append(ceoDiv);
-      // Storing the company logo
-      const stockDiv = $('<div>').addClass('stock');
-      const companyLogo = response.logo.url;
-      const logoHolder = $(`<img src="${companyLogo}">`);
-      stockDiv.append(logoHolder);
-      // Storing the company name
-      const companyName = response.quote.companyName;
-      const nameHolder = $('<h3>').text(`${companyName}`);
-      stockDiv.append(nameHolder);
-      // Storing the price and change
-      const stockPrice = response.quote.latestPrice;
-      const change = response.quote.change;
-      const priceHolder = $('<h6>').text(`Price: $${stockPrice} `);
-      if (parseFloat(change) >= 0) {
-        priceHolder.append(`<span id="change" style="color:green">  +${change}</span>`);
-      } else {
-        priceHolder.append(`<span id="change" style="color:red">  ${change}</span>`);
-      }
-      priceHolder.append(`<small>(IEX real-time price)</small>`);
-      stockDiv.append(priceHolder);
-      // Storing the 10 news summary
-      for (let i = 0; i < response.news.length; i++) {
-        const companyNews = response.news[i].summary;
-        const summaryHolder = $('<p>').text(`${companyNews}`);
-        stockDiv.append(summaryHolder);
-      }
-      // Adding the stockDiv to the DOM
-      $('#stocks-view').prepend(stockDiv);
-    });
-  }
-  // this function display all the buttons
+  // display all the symbol buttons
   const render = function () {
     $('#buttons-view').empty();
 
@@ -78,25 +31,22 @@ $(document).ready(function () {
       $('#buttons-view').append(newButton);
     }
   }
+  render();
 
-  const inStocsList = function (stock) {
+  const isValidSymbol = function (stock) {
+    return (validationList.includes(stock)) ? true : false;
+  }
+
+  const isInStocksList = function (stock) {
     let result = (stocksList.includes(stock)) ? true : false;
     return result;
   }
 
-  const isValidSymbol = function (stock) {
-    if (validationList.includes(stock)) {
-      return true;
-    } else {
-      return false;
-    }
-  }
   // This function handles events when add stock button is clicked
   const addButton = function (e) {
     e.preventDefault();
     const stock = $('#stock-input').val().trim().toUpperCase();
-
-    if (isValidSymbol(stock) && !inStocsList(stock)) {
+    if (isValidSymbol(stock) && !isInStocksList(stock)) {
       stocksList.push(stock);
       render();
     } else {
@@ -104,9 +54,72 @@ $(document).ready(function () {
     }
     $('#stock-input').val('');
   }
-
   $('#add-stock').on('click', addButton);
-  $('#buttons-view').on('click', '.stock-btn', displayStockInfo);
 
-  render();
+  const displayCEO = function (companyCEO) {
+    const ceoDiv = $('<div>');
+    const ceoHolder = $('<small>').text(`CEO: ${companyCEO}`);
+    ceoDiv.append(ceoHolder);
+    $('#ceo-view').append(ceoDiv);
+  }
+
+  const storeLogo = function (stockDiv, companyLogo) {
+    const logoHolder = $(`<img src="${companyLogo}">`);
+    stockDiv.append(logoHolder);
+  }
+
+  const storeCompanyName = function (stockDiv, companyName) {
+    const nameHolder = $('<h3>').text(`${companyName}`);
+    stockDiv.append(nameHolder);
+  }
+
+  const storePrice = function (stockDiv, stockPrice, change) {
+    const priceHolder = $('<h6>').text(`Price: $${stockPrice} `);
+    if (parseFloat(change) >= 0) {
+      priceHolder.append(`<span id="change" style="color:green">  +${change}</span>`);
+    } else {
+      priceHolder.append(`<span id="change" style="color:red">  ${change}</span>`);
+    }
+    priceHolder.append(`<small>(IEX real-time price)</small>`);
+    stockDiv.append(priceHolder);
+  }
+
+  const storeNews = function (stockDiv, news) {
+    console.log(news);
+    for (let i = 0; i < news.length; i++) {
+      const headline = news[i].headline;
+      const companyNews = news[i].summary;
+      const summaryHolder = $('<div>').addClass('news');
+      summaryHolder.append(`<p><strong>Headline:</strong> ${headline}</p><hr><p>${companyNews}</p>`);
+      stockDiv.append(summaryHolder);
+    }
+  }
+
+  // renderCompanyInfo function renders the HTML to display the selected company content
+  const renderCompanyInfo = function (response) {
+    const stockDiv = $('<div>').addClass('stock');
+    displayCEO(response.company.CEO);
+    storeLogo(stockDiv, response.logo.url);
+    storeCompanyName(stockDiv, response.quote.companyName);
+    storePrice(stockDiv, response.quote.latestPrice, response.quote.change);
+    storeNews(stockDiv, response.news);
+    $('#stocks-view').prepend(stockDiv);
+  }
+
+  const getStockInfo = function () {
+    $('#ceo-view').empty();
+    $('#stocks-view').empty();
+    const stock = $(this).attr('data-name');
+    // Grab the stock symbol from the button clicked and add it to the queryURL
+    const queryURL = `https://api.iextrading.com/1.0/stock/${stock}/batch?types=logo,company,quote,news&range=1m&last=10`;
+
+    $.ajax({
+      url: queryURL,
+      method: 'GET'
+    }).then(function (response) {
+      renderCompanyInfo(response);
+    });
+  }
+  $('#buttons-view').on('click', '.stock-btn', getStockInfo);
+
 });
