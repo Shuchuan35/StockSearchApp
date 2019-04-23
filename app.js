afc77154c70aaf76125023b1512e7e316f70d0ba
scream <hou.jennifer35@gmail.com>
2019-04-23 16:41:11 -0400

    refactor renderCompanyInfo function

--- app.js
+++ app.js
@@ -20,84 +20,51 @@ $(document).ready(function () {
   const render = function () {
     $('#buttons-view').empty();
     for (let i = 0; i < stocksList.length; i++) {
-      const newButton = $('<button class="btn btn-info">');
-      newButton.addClass('stock-btn');
-      newButton.attr('data-name', stocksList[i]);
-      newButton.text(stocksList[i]);
+      const newButton = $('<button class="btn btn-info">')
+        .addClass('stock-btn')
+        .attr('data-name', stocksList[i])
+        .text(stocksList[i]);
       $('#buttons-view').append(newButton);
     }
   }
   render();
 
-  const isValidSymbol = function (stock) {
-    return (validationList.includes(stock)) ? true : false;
-  }
-
-  const isInStocksList = function (stock) {
-    let result = (stocksList.includes(stock)) ? true : false;
-    return result;
-  }
-
-  // This function handles events when add stock button is clicked
-  const addButton = function (e) {
-    e.preventDefault();
-    const stock = $('#stock-input').val().trim().toUpperCase();
-    if (isValidSymbol(stock) && !isInStocksList(stock)) {
-      stocksList.push(stock);
-      render();
-    } else {
-      alert('Try again! Either exists or invalid Symbol!');
-    }
-    $('#stock-input').val('');
-  }
-  $('#add-stock').on('click', addButton);
-
-  const displayCEO = function (companyCEO) {
-    const ceoDiv = $('<div>');
-    const ceoHolder = $('<small>').text(`CEO: ${companyCEO}`);
-    ceoDiv.append(ceoHolder);
-    $('#ceo-view').append(ceoDiv);
-  }
-
-  const storeLogo = function (stockDiv, companyLogo) {
-    const logoHolder = $(`<img src="${companyLogo}">`);
-    stockDiv.append(logoHolder);
-  }
-
-  const storeCompanyName = function (stockDiv, companyName) {
-    const nameHolder = $('<h3>').text(`${companyName}`);
-    stockDiv.append(nameHolder);
-  }
-
-  const storePrice = function (stockDiv, stockPrice, change) {
-    const priceHolder = $('<h6>').text(`Price: $${stockPrice} `);
+  const displayPrice = function (stockPrice, change) {
+    const priceTag = $('<h6>').text(`Price: $${stockPrice} `);
     if (parseFloat(change) >= 0) {
-      priceHolder.append(`<span id="change" style="color:green">  +${change}</span>`);
+      priceTag.append(`<span id="change" style="color:green">  +${change}</span>`);
     } else {
-      priceHolder.append(`<span id="change" style="color:red">  ${change}</span>`);
+      priceTag.append(`<span id="change" style="color:red">  ${change}</span>`);
     }
-    priceHolder.append(`<small>(IEX real-time price)</small>`);
-    stockDiv.append(priceHolder);
+    priceTag.append(`<small>(IEX real-time price)</small>`);
+    return priceTag;
   }
 
   const storeNews = function (stockDiv, news) {
     // console.log(news);
     for (let i = 0; i < news.length; i++) {
-      const headline = news[i].headline;
-      const companyNews = news[i].summary;
-      const summaryHolder = $('<div>').addClass('news');
-      summaryHolder.append(`<p><strong>Headline:</strong> ${headline}</p><hr><p>${companyNews}</p>`);
-      stockDiv.append(summaryHolder);
+      const headline = $('<strong>').text(news[i].headline);
+      const companyNews = $('<p>').text(news[i].summary);
+      const newsDiv = $('<div>').addClass('news')
+        .append(headline, $('<hr>'), companyNews);
+      stockDiv.append(newsDiv);
     }
   }
 
   // renderCompanyInfo function renders the HTML to display the selected company content
   const renderCompanyInfo = function (response) {
-    const stockDiv = $('<div>').addClass('stock');
-    displayCEO(response.company.CEO);
-    storeLogo(stockDiv, response.logo.url);
-    storeCompanyName(stockDiv, response.quote.companyName);
-    storePrice(stockDiv, response.quote.latestPrice, response.quote.change);
+
+    const ceoDiv = $('<div>')
+      .append($('<small>').text(`CEO: ${response.company.CEO}`));
+    $('#ceo-view').append(ceoDiv);
+
+    const stockDiv = $('<div>').addClass('stock')
+      .append(
+        $('<img>').attr('src', response.logo.url),
+        $('<3>').text(response.quote.companyName),
+        displayPrice(response.quote.latestPrice, response.quote.change)
+      );
+
     storeNews(stockDiv, response.news);
     $('#stocks-view').prepend(stockDiv);
   }
@@ -112,10 +79,30 @@ $(document).ready(function () {
     $.ajax({
       url: queryURL,
       method: 'GET'
-    }).then(function (response) {
-      renderCompanyInfo(response);
-    });
+    }).then(renderCompanyInfo);
   }
+
+  const isValidSymbol = function (stock) {
+    return (validationList.includes(stock)) ? true : false;
+  }
+
+  // This function handles events when add stock button is clicked
+  const addButton = function (e) {
+    e.preventDefault();
+    const stock = $('#stock-input').val().trim().toUpperCase();
+    if (isValidSymbol(stock) && !stocksList.includes(stock)) {
+      stocksList.push(stock);
+      render();
+    } else {
+      alert('Try again! Either exists or invalid Symbol!');
+    }
+    $('#stock-input').val('');
+  }
+
+  //==================================
+  // Event Listeners
+  //==================================
+  $('#add-stock').on('click', addButton);
   $('#buttons-view').on('click', '.stock-btn', getStockInfo);
 
 });
